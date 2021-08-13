976d29e078b79adae95a247c7ead4a024346abc5
Thomas Almeida <thomas.rodrigues@idealtrends.com.br>
2021-08-13 14:49:24 -0300

    Otimizando Scripts

--- scripts/carregarUrl.js
+++ scripts/carregarUrl.js
@@ -1,18 +1,30 @@
-var getUrl = document.getElementById('getUrl')
 var inputHidden = document.getElementById('hidden')
 
-//form
+//form components
 var nome = document.getElementById('txtNome')
 var email = document.getElementById('txtEmail')
 var btn = document.getElementById('enviar')
 
 btn.addEventListener('click', function() {
 
-    //receber dados
-    inputHidden.value = localStorage.getItem('Back_URL')
+    if (nome.value == "" || email.value == "") {
+        alert('Preencha todos os campos')
+    } else {
 
-    // Enviar dados
-    console.log('Nome: ' + nome.value)
-    console.log('Email: ' + email.value)
-    console.log('Destino: ' + inputHidden.value + "/" + window.location.href.slice(22, 32))
+        //receber dados
+        inputHidden.value = localStorage.getItem('Back_URL')
+
+        // Enviar dados
+        console.log('Nome: ' + nome.value)
+        console.log('Email: ' + email.value)
+        console.log('Destino: ' + inputHidden.value + "/" + window.location.href.slice(22, 32))
+
+        alert('Os resultados apareceram no console 😎')
+
+        //limpar campos
+        nome.value = ""
+        email.value = ""
+    }
+
+
 })
