10b92ed46c065d167c3c85e59b9b17c8fde49708
Thomas Almeida <thomas.rodrigues@idealtrends.com.br>
2021-08-13 13:18:07 -0300

    Otimização para apresentação

---
+++ scripts/receberUrl.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var setUrl = document.getElementById('setUrl')
+
+
+function getURL() {
+    setUrl = window.location.href
+    localStorage.setItem('Back_URL', setUrl);
+    console.log('URL PEGA: ' + setUrl)
+}
